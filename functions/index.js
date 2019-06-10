658ebbf07e78fa22c3c7b64a16f04690feae458c
Patrick Vestal <patrick.vestal@gmail.com>
2019-06-10 18:16:49 +0000

    added cloud function to reset game count

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Listen for updates to any 'turn' document.
+exports.resetGame = functions.firestore
+    .document('game/turn')
+    .onUpdate((change, context) => {
+      // Retrieve the current and previous value
+      const data = change.after.data();
+      const previousData = change.before.data();
+
+      // We'll only update if another user submits.
+      if (data.currentUser === previousData.currentUser) return null;
+      
+      let gameStatus = data.gameStatus;
+      let modCount = data.count;
+
+      if (modCount > 9) {
+          gameStatus = "reset"
+          modCount = 0
+      } else {
+          gameStatus = "inprogress"
+      }
+
+      // Then return a promise of an update operation to update firestore
+      return change.after.ref.update({
+        gameStatus: gameStatus,
+        count: modCount
+      }, {merge: true});
+    });
